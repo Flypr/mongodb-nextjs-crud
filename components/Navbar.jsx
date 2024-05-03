b86341c495e9cca9d0a1d6d6de251daf3ec2fee8
Filip Mihocas <filip.mihocas@gmail.com>
2024-05-03 16:34:52 +0300

    Update Navbar component with new styles and links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,9 +3,19 @@ import Link from 'next/link';
 
 const Navbar = () => {
 	return (
-		<nav>
-			<Link href={'/'}>Home</Link>
-			<Link href={'/addTopic'}>About</Link>
+		<nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+			<Link
+				className="text-white font-bold"
+				href={'/'}
+			>
+				Home
+			</Link>
+			<Link
+				className="bg-white p-2"
+				href={'/addTopic'}
+			>
+				About
+			</Link>
 		</nav>
 	);
 };
